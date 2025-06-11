26138ed0392e44c836a94a512420fad5c4c105da
DaveElPato <166331271+DaveElPato@users.noreply.github.com>
2025-06-11 12:03:35 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 docker compose up --build -d
 docker cp . mongo:/data
+docker exec -ti mongo bash
+cd data
+./resources/import_distances.sh
 docker exec -ti mongo /data/resources/import_distances.sh
