00361e6c9cbf14683c96ff8107fd4a91bfb31677
DaveElPato <166331271+DaveElPato@users.noreply.github.com>
2025-06-11 12:01:55 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker compose up --build -d
-docker cp ./data/origin_dest_distances.jsonl mongo:/data
+docker cp . mongo:/data
 docker exec -ti mongo /data/resources/import_distances.sh
