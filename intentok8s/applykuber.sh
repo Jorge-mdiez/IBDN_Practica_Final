82578a8a3fc8a31f153a45eca934f83a21878cf9
DaveElPato <166331271+DaveElPato@users.noreply.github.com>
2025-06-11 12:40:42 +0200

    Update applykuber.sh

--- intentok8s/applykuber.sh
+++ intentok8s/applykuber.sh
@@ -8,3 +8,6 @@ kubectl apply -f flask-dep.yaml
 kubectl apply -f flask-ser.yaml
 kubectl apply -f proxy-dep.yaml
 kubectl apply -f proxy-ser.yaml
+
+#Para que podamos abrirlo en localhost ya que es tipo ClusterIP y no es accesible desde fuera del clúster
+kubectl port-forward svc/flask-app 5001:5001
